f5487a0641fa7f10f0ab7fcd1cee83389db02ca1
Golam Rasul Maruf <120311809+GRMaruf@users.noreply.github.com>
2026-07-03 06:30:09 +0000

    Add: Docker compose

--- myProject/core/settings.py
+++ myProject/core/settings.py
@@ -77,11 +77,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', # Fixed for PostgreSQL
+        'NAME': 'POSTGRES_DB', # Your database name
+        'USER': 'POSTGRES_USER', # The user who is assigned to the database
+        'PASSWORD': 'POSTGRES_PASSWORD', # User password
+        'HOST': 'localhost', # Use your database domain (domain.com), if using other hosting site for database
+        'PORT': '5432', # Fixed for PostgreSQL
     }
+        # For Production use, it is recommended to use environment variables for 'NAME', 'USER', 'PASSWORD' and 'HOST'
 }
 
 
